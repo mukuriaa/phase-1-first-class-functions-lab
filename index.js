3455264e708577bb3546cd071bab5f9181c477b5
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-09 06:33:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const drivers = ['Eric', 'Eden', 'Lebron', 'Steph']
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers =function (drivers){
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiple){
+    return function (fare){
+        return fare * multiple;
+
+    }
+}
+
+function fareDoubler(fare) {
+    return fare* 2;
+  }
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+
+  function selectDifferentDrivers(drivers, fun){
+    return fun(drivers);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
